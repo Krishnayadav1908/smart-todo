cfa077bfad050a26ce6037a27703e0cb417d8189
Krishna Yadav <krishanayadav9999@gmail.com>
2026-01-06 16:08:26 +0530

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,46 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])  // State to hold the list of todos
+
+  // Function to add a new todo
+
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => [ { id: Date.now(), ...todo},...prevTodos])   // Add new todo at the beginning of the list  
+  }
+
+// Function to update an existing todo by id
+  const updateTodo = (id,todo) => {
+  setTodos((prev) =>
+    prev.map((prevTodos) => (prevTodos.id === id ? todo : prevTodos))  // Update the matching todo
+  )
+  }
+
+  // Function to remove a todo by id
+const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id)) // Remove the todo with the matching id
+  }
+
+// Function to toggle the completion status of a todo by id
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed:!prevTodo.completed}: prevTodo)   // Toggle the completed status
+  )
+  }
 
   return (
-    <>
-   <h1 className="text-3xl font-bold underline text-blue-500">
-    Hello world!
-  </h1>
-    </>
+    <TodoProvider value={{ todos, addTodo, updateTodo,deleteTodo, toggleComplete }}>
+ <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </TodoProvider>
   )
 }
 
