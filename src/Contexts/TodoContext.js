970ab16a330742aefe404324bcb92b7986c0a551
Krishna Yadav <krishanayadav9999@gmail.com>
2026-01-06 08:48:27 +0530

    initial setup and created context folder

---
+++ src/Contexts/TodoContext.js
@@ -0,0 +1,20 @@
+import React, { createContext } from 'react'
+
+export const TodoContext = createContext(   // Creating a Context for Todo management
+    {
+        todos: [
+            { id: 1, todo: 'Sample Todo', completed: false }, // Example todo item
+        ],
+        addTodo: (todo,id) => {},   // Function to add a new todo
+        updateTodo: (todo, id) => {},      // Function to update an existing todo
+        toggleComplete: (id) => {}, // Function to toggle the completion status of a todo
+        deleteTodo: (id) => {},  // Function to delete a todo
+    }
+);
+
+
+export const useTodo = () => {
+    return useContext(TodoContext);     // Custom hook to use the TodoContext
+}
+
+export const TodoProvider = TodoContext.Provider // Exporting the Provider component of the TodoContext
